f1dfe565e1225242e048c8d2c7dd6a49ee24fe7e
skellybob56 <lucas.chowney@outlook.com>
2026-02-12 22:01:09 +0000

    Add early plans for water tileset rules

---
+++ plan/water_tileset_constraints.py
@@ -0,0 +1,17 @@
+when WATER tile state is caused:
+
+ 0 - ?
+ 1 - ah == av == DEEP_WATER
+ 2 - (ah == SAND) or 
+ 	(ah > SAND and )
+ 3 - 
+ 4 - 
+ 5 - 
+ 6 - 
+ 7 - 
+ 8 - 
+ 9 - 
+10 - 
+11 - 
+12 - 
+13 - 
